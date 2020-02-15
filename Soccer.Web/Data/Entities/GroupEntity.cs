56b853c9f89eb120d9d11e81d5a424242590bb55
Oscar Doria <oscardoria244956@correo.itm.edu.com>
2020-02-15 17:24:44 -0500

    Add tables to the database and create a unique index in teams

--- Soccer.Web/Data/Entities/GroupEntity.cs
+++ Soccer.Web/Data/Entities/GroupEntity.cs
@@ -3,15 +3,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Soccer.Web.Data.Entities
 {
-    public class TeamEntity
+    public class GroupEntity
     {
         public int Id { get; set; }
 
-        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string Name { get; set; }
 
-        public string LogoPath { get; set; }
+        public TournamentEntity Tournament { get; set; }
+
         public ICollection<GroupDetailEntity> GroupDetails { get; set; }
+        public ICollection<MatchEntity> Matches { get; set; }
     }
 }
